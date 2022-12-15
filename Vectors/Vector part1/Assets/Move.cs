e90fb59ee4eb5ea11fa17f6337c845687b573bc5
yevgenylev <zhenya.levchenko2002@gmail.com>
2022-12-15 21:58:06 +0200

    Vectors
    Vectors

---
+++ Vectors/Vector part1/Assets/Move.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+public class Move : MonoBehaviour {
+
+    public Vector3 goal = new Vector3(0, 0, 4);
+    public float speed = 2f;
+
+    void Start() {
+    }
+
+    void Update() {
+       this.transform.Translate(goal.normalized*speed*Time.deltaTime);
+    }
+}
---
+++ Vectors/Vectors part2/Assets/MoveToGoal.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class MoveToGoal : MonoBehaviour {
+
+    // Object speed
+    public float speed = 2.0f;
+    // Accuracy
+    public float accuracy = 0.01f;
+    // Object target
+    public Transform goal;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate() {
+
+        // Calculate the facing direction
+        this.transform.LookAt(goal.position);
+
+        // Find the the direction vector
+        Vector3 direction = goal.position - this.transform.position;
+
+        // Draw debug ray in the direction your object is facing
+        Debug.DrawRay(this.transform.position, direction, Color.red);
+
+        // Check how close you character is to the target
+        if (direction.magnitude > accuracy) {
+
+            // Move the Object
+            this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        }
+    }
+}
